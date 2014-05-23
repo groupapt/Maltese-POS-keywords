6776b9e1684b2588988ff9a14f3435a5aea75d0d
Daniel <desiradaniel2007@hotmail.com>
2014-05-24 00:54:41 +0200

    Now using Maltese POS Tagger; Eliminated >100 lines of useless code

--- keywords.py
+++ keywords.py
@@ -1,162 +1,22 @@
 __author = 'Daniel'
 
-import re
-
-#added dictionary as per Jurgen's advice
-word_dict = {
-	'a': [],
-	'b': [],
-	'c': [],
-	'd': [],
-	'e': [],
-	'f': [],
-	'g': [],
-	'h': [],
-	'i': [],
-	'j': [],
-	'k': [],
-	'l': [],
-	'm': [],
-	'n': [],
-	'o': [],
-	'p': [],
-	'q': [],
-	'r': [],
-	's': [],
-	't': [],
-	'u': [],
-	'v': [],
-	'w': [],
-	'x': [],
-	'y': [],
-	'z': [],
-	'other': []
-}
-
-'''
-' Class to help deal with word frequancy
-' @property image String representing word
-' @property occurance Number of times word is found
-'''
-class Word:
-	'''
-	' Constructor
-	' @param image Word string value
-	'''
-	def __init__(self, image):
-		self.image = image
-		self.occurance = 1
-
-	'''
-	' Increments the occurance of a word by 1
-	'''
-	def inc_occurance(self):
-		self.occurance += 1
-
-	'''
-	' @param other Other object
-	'''
-	def __lt__(self, other):
-		return self.occurance < other.occurance
-
-	'''
-	' @param other Other object
-	'''
-	def __gt__(self, other):
-		return self.occurance > other.occurance
-
-	'''
-	' @param other Other object
-	'''
-	def __eq__(self, other):
-		return self.image == other.image
-
-'''
-' Looks for a given word in a list
-' @param words_captured List holding words to search through
-' @param needle Word string literal to search for
-' @return Word object if word found or False otherwise
-'''
-def word_found(needle):
-	for word in word_dict[dict_key(needle)]:
-		if needle in word.image:
-			return word
-	return False
-
-'''
-' Strips away any of the given occurances found in a given string
-' @param list List of strings to be striped
-' @param text String to process
-' @return String with all occurances stripped away
-'''
-def strip_matches(list, text):
-	for x in list:
-		text = text.replace(x, '')
-	return text
-
-'''
-' Split a string into a list of words
-' @param text String to split
-' @return List of strings representing words
-'''
-def get_words(text):
-	text = text.replace('\n', ' ')
-	return text.split(' ')
-
-'''
-' Remove words starting with a digit from a list of word strings
-' @param list List of word strings
-' @return Resulting list
-'''
-def strip_digit_words(list):
-	for word in list:
-		if word != '' and re.match('[1-9]', word[0]):
-			list.remove(word)
-	return list
-
-'''
-' Returns a key depending on the first character of the word passed
-' @param word String representing a word
-' @return Key representing the position of the word in word_dict
-'''
-def dict_key(word):
-	if re.match('[a-z, A-Z]', word):
-		return word[0].lower()
-	else:
-		return 'other'
+from collections import defaultdict
+from SOAPpy import SOAPProxy
+from operator import itemgetter
 
 '''
 ' Diagnoses a piece of text and gives back the 5 most common keywords
 ' @param text Text to process
 ' @return List of 5 Word objects representing the most common keywords
 '''
+
+
 def generate_keywords(text):
-	punctuation = ['.', ',', '?', ':', ';', '!', '"', '\'', '(', ')']
-	simple_words = ['huwa', 'hija', 'bin', 'imma', 'u', 'ghal', 'illi', 'dan', 'dina', 'sobordinament', 'preliminarjament', 'li', 'il-']
+	server = SOAPProxy('http://metanet4u.research.um.edu.mt/services/MtPOS?wsdl')
+	words = [w[:w.rfind('_')] for w in server.tagParagraphReturn(text).split(' ') if w[len(w)-2:] == 'NN']
 
-	text = strip_matches(punctuation, text)
-	text = strip_matches(simple_words, text)
-
-	words = strip_digit_words(get_words(text))
-
+	word_dict = defaultdict(int)
 	for word in words:
-		word_captured = word_found(word)
-		if not word_captured:
-			word_dict[dict_key(word)].append(Word(word))
-		else:
-			word_captured.inc_occurance()
+		word_dict[word] += 1
 
-	top_five_words = []
-	for x in range(5):
-		max_occurance = 0
-		most_common = None
-		for list in word_dict:
-			if word_dict[list]:
-				most_common_current = max(word_dict[list])
-				if most_common_current.occurance > max_occurance:
-					max_occurance = most_common_current.occurance
-					most_common = most_common_current
-		top_five_words.append(most_common)
-		word_dict[dict_key(most_common.image)].remove(most_common)
-
-	return top_five_words
+	return [x[0] for x in (sorted(word_dict.iteritems(), key=itemgetter(1), reverse=True)[:5])]
